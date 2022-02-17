7a95f45559a1822fecca4cac727eee499d983a1e
rogerio machado <rogerio.s.machado@gmail.com>
2022-02-17 20:10:16 -0300

    componentizado

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,36 @@
-export function SideBar() {
+import { Button } from './Button';
+import { GenreResponseProps } from './../App';
+
+interface IProps {
+  genres: GenreResponseProps[];
+  selectedGenreId: number;
+  handleClickButton: Function;
+}
+
+export function SideBar({
+  genres,
+  selectedGenreId,
+  handleClickButton,
+}: IProps) {
   // Complete aqui
+
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
